0008ed3e5702e525ea5507ac07b95b5f0889b074
iamshyamganatra <shyam2010ganatra@gmail.com>
2020-10-23 20:14:21 +0530

    updated the script to capture two images

--- software/Motor_Control_Scripts/Step1_Camera_Rasp_pi_wKeyBoardInput.py
+++ software/Motor_Control_Scripts/Step1_Camera_Rasp_pi_wKeyBoardInput.py
@@ -1,12 +1,8 @@
 '''
 This is a code that runs on Rasp pi.
-
 What it does: Allowing the user to input keyboard commands and communicate
 them (say number 1 or 0) over I2C.
-
 Rasp Pi controls the Arduino
-
-
 '''
 from picamera import PiCamera
 from time import sleep
@@ -19,27 +15,28 @@ addr = 0x8 # bus address
 bus = SMBus(1) # indicates /dev/ic2-1
 
 numb = 1
-
+camera = PiCamera()
 print ("Enter 1 for ON or 0 for OFF")
 while numb == 1:
 
         ledstate = input(">>>>   ")
 
         if ledstate == "1":
+                camera.start_preview(alpha=200) # Add alpha parameter to make it slightly see through in ops - adjust to different resolution
+                sleep(5) # at least 2 seconds so that camera can sense lighting conditions
+                camera.capture('testimage_loop1.jpg') # change location and file name, if needed
+                camera.stop_preview()
                 bus.write_byte(addr, 0x1) # switch it on
+
+
         elif ledstate == "0":
                 bus.write_byte(addr, 0x0) # switch it on
+
+                camera.start_preview(alpha=200) # Add alpha parameter to make it slightly see through in ops - adjust to different resolution
+                sleep(5) # at least 2 seconds so that camera can sense lighting conditions
+                camera.capture('testimage_loop2.jpg') # change location and file name, if needed
+                camera.stop_preview()
         else:
                 numb = 0
-
-
-
-camera = PiCamera()
-#camera.rotation = 90 # comment out if your camera is in correct orientation
-
-camera.start_preview(alpha=200) # Add alpha parameter to make it slightly see through in ops - adjust to different resolution
-sleep(5) # at least 2 seconds so that camera can sense lighting conditions
-camera.capture('testimage.jpg') # change location and file name, if needed
-camera.stop_preview()
 
 sys.exit() # esc in case of issues, added to make sure no system hang
