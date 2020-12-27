300f5e6056a4d2fd4a0b94f212a0f0e5f5f69077
Phillip Chin <ekkus93@gmail.com>
2020-12-27 12:46:11 -0800

    added script for installing system level dependences for cl_reachy

---
+++ software/cl_reachy/install_apt_pkgs.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/bash
+
+# utilities
+apt-get install screen htop less zip unzip -y
+
+# editors
+apt-get install emacs vim -y
+
+# rdp
+apt-get install xrdp -y
+adduser xrdp ssl-cert
+
+# cheese
+apt-get install cheese -y
+
+# mosquitto
+apt-get install software-properties-common -y
+apt-get update
+apt-get install mosquitto mosquitto-clients -y
+apt clean
+
+# swig
+apt-get install -y swig
+
+# sound dev
+apt-get install libpulse-dev -y
+apt-get install libasound2-dev -y
+apt-get install pavucontrol -y
+
+# coral tpu
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | tee /etc/apt/sources.list.d/coral-edgetpu.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+apt-get update
+apt-get install libedgetpu1-std -y
+
+# numpy dependencies
+apt-get install libatlas-base-dev -y
